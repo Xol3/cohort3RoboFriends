711457a1695399095eb82fe4a5b73b397cdb0fed
pancho <xoalxiyetl@gmail.com>
2019-03-10 16:15:38 -0600

    going to start working on the robo friends app

---
+++ sonideros/src/App.js
@@ -0,0 +1,32 @@
+//react is being distructured and we are importong the component
+import React, { Component } from 'react';
+//import the logo
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ sonideros/src/MyComponent.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './MyComponent.css';
+
+class MyComponent extends Component{
+  render(){ // this reander my new component
+    return(
+      //the return should only return one item
+      <div className='f2 tc'>
+        <h1>OHH YEAHH MR.pancho, WEEEPPAAA</h1>
+        <p>YO MAESTRO EL UNICO en tu vida </p>
+        <p>{this.props.cumbia}</p>
+      </div>
+    )
+  }
+}
+
+export default MyComponent;
